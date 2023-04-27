d1aa3da8fde6a2277e5baefe25e173eaf1b989ff
Gwhyyy <work@gwhyyy.com>
2023-04-27 23:48:34 +0100

    [ Add ] adde more examples

---
+++ example/get_pubkey_from_identifier_nip_05__2.dart
@@ -0,0 +1,14 @@
+import 'package:dart_nostr/dart_nostr.dart';
+
+void main() async {
+  try {
+    final publicKeyFromNip05 =
+        await Nostr.instance.relaysService.pubKeyFromIdentifierNip05(
+      internetIdentifier: "jb55@jb55.com",
+    );
+
+    print("publicKeyFromNip05: $publicKeyFromNip05"); // ...
+  } catch (e) {
+    print(e.toString());
+  }
+}
---
+++ example/verify_nip05.dart
@@ -0,0 +1,16 @@
+import 'package:dart_nostr/dart_nostr.dart';
+
+void main() async {
+  final publicKeyToCheckWith =
+      "32e1827635450ebb3c5a7d12c1f8e7b2b514439ac10a67eef3d9fd9c5c68e245";
+
+  final isIdentifierVerifiedWithPublixKey =
+      await Nostr.instance.relaysService.verifyNip05(
+    internetIdentifier: "jb55@jb55.com",
+    pubKey: publicKeyToCheckWith,
+  );
+
+  print(
+    "isIdentifierVerifiedWithPublixKey: $isIdentifierVerifiedWithPublixKey",
+  );
+}
